c8e87fb8b7790e5a1e01b1854990e827fb96adef
Adam Ausburn <acausbu@gmail.com>
2022-06-29 20:19:08 -0500

    identified cleanup to make repo public

--- apps/infinity/registry/AllowInsecureGuestAuth/install.cmd
+++
@@ -1,3 +0,0 @@
-if not exist "C:\ProgramData\AutoPilotConfig" md "C:\ProgramData\AutoPilotConfig"
-xcopy "AllowInsecureGuestAuthScript.ps1" "C:\ProgramData\AutoPilotConfig" /Y
-Powershell.exe -Executionpolicy bypass -File "C:\ProgramData\AutoPilotConfig\AllowInsecureGuestAuthScript.ps1"
